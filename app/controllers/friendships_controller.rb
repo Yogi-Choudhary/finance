2c6d54e5bf7bd23af8d3d01a63fe53dfeffbcf1e
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2021-04-30 15:17:20 +0530

    create_friendship_tracking_from_browser

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,14 @@
 class FriendshipsController < ApplicationController
 
   def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "Following friend"
+    else
+      flash[:alert] = "There was something wrong with the tracking request"
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
@@ -9,5 +17,5 @@ class FriendshipsController < ApplicationController
     flash[:notice] = "Stopped following"
     redirect_to my_friends_path
   end
-  
+
 end
