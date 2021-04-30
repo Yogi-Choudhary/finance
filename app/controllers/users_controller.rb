17155bba77d50a6fd9e3dc4cb91afdd0c3835f96
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2021-04-30 14:39:39 +0530

    add_search_friends_form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,13 @@ class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
-  
+
   def my_friends
     @friends = current_user.friends
+  end
+
+  def search
+    render json: params[:friend] 
   end
 
 end
